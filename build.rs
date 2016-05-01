c4d8bb4658ed3867525dceb4967971a10ecc600a
Martin Pool <mbp@sourcefrog.net>
2016-05-01 15:58:04 -0700

    Fix comment

--- build.rs
+++ build.rs
@@ -1,7 +1,8 @@
 use std::env;
 
 pub fn main() {
-    // Make the OUT_DIR from build time available at run time.
+    // Make the PROFILE from build time available at run time, so that C tests
+    // can link against the debug or release library.
     if let Ok(profile) = env::var("PROFILE") {
         println!("cargo:rustc-cfg=profile={:?}", profile);
     }
