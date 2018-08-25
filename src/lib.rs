41a4ccd36a82b8cbf825aed60e77566bcb408859
Martin Pool <mbp@sourcefrog.net>
2018-08-24 23:06:34 -0700

    Add some rustdoc.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,14 @@
 // librdiff(rust) -- library for network deltas
 // Copyright 2015, 2016, 2018 Martin Pool.
 
+//! Compute and apply deltas from a file signature to a new file.
+//!
+//! This uses the same algorithm (but not the same format) as `rsync`,
+//! and the same algorithm and format as `rdiff`.
+//!
+//! Homepage: <https://github.com/sourcefrog/librdiff-rs>.
+
 mod rollsum;
 
+/// Semver string for this library.
 pub static VERSION: &'static str = env!("CARGO_PKG_VERSION");
